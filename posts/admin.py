21648cb34a4b3957655ead2fef082c26abb1b996
salayevsaleh <salehsalayev@Saleh-MacBook-Pro.local>
2023-01-30 14:56:14 +0400

    create project

---
+++ posts/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from . import models
+from .models import Author, Category, Post , Comment
+
+admin.site.register(Author)
+admin.site.register(Category)
+admin.site.register(Post)
+admin.site.register(Comment)
+
+
+
